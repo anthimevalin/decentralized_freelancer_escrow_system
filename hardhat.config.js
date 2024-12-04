130ff2dd0e92936c0c20a1591bea4d4cac1129d7
davidalesch <david.alesch.24@ucl.ac.uk>
2024-12-04 18:47:02 +0000

    FinalTest and some data analysis

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      accounts: {
+        count: 150, // Increase the number of generated accounts
+      },
+    },
+  },
 };
